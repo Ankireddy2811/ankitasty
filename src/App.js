9a692874247ed7cabea8493376561a48357aa109
Ankireddy2811 <ankireddyputtam@gmail.com>
2023-12-22 13:32:38 +0000

    add cart

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import {Switch, Route} from 'react-router-dom'
+import {Component} from "react"
+import {Switch, Route, BrowserRouter} from 'react-router-dom'
 
 import Login from './components/Login'
 
 import ProtectedRoute from './components/ProtectedRoute'
 
 import Home from './components/Home'
+
 import EachRestaurant from './components/EachRestaurant'
+
+import CartContext from './context/CartContext'
 
 import './App.css'
 
-const App = () => (
-  <Switch>
-    <Route exact path="/login" component={Login} />
-    <ProtectedRoute exact path="/" component={Home} />
-    <ProtectedRoute exact path="/restaurant/:id" component={EachRestaurant} />
-  </Switch>
-)
+class App extends Component{
+    state = {cartList: []}
+    render(){
+        return(
+         <BrowserRouter>
+       <CartContext.Provider value={{}}>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <ProtectedRoute exact path="/" component={Home} />
+        <ProtectedRoute
+          exact
+          path="/restaurant/:id"
+          component={EachRestaurant}
+        />
+      </Switch>
+    </CartContext.Provider>
+  </BrowserRouter>
+        )
+    }
+  
+}
+  
+
 
 export default App
--- src/components/EachRestaurantFoodItem/index.js
+++ src/components/EachRestaurantFoodItem/index.js
@@ -1,30 +1,96 @@
+import {Component} from 'react'
+import CartContext from '../../context/CartContext'
+// import Counter from '../Counter'
 import './index.css'
 
-const EachRestaurantFoodItem = props => {
-  const {eachContent} = props
-  return (
-    <li className="each-restaruant-food-list-item">
-      <img
-        src={eachContent.imageUrl}
-        alt={eachContent.name}
-        className="each-food-item-image"
-      />
-      <div className="item-details">
-        <h2 className="item-name">{eachContent.name}</h2>
-        <p className="item-cost">₹ {eachContent.cost}</p>
-        <span className="item-rating-container">
-          <img
-            src="https://res.cloudinary.com/dcqt2hg87/image/upload/v1703165215/star_rktiju.png"
-            alt="rating"
-          />
-          <h2 className="item-rating">{eachContent.rating}</h2>
-        </span>
-        <button className="add-button" type="button">
-          ADD
-        </button>
-      </div>
-    </li>
-  )
+class EachRestaurantFoodItem extends Component {
+  state = {itemCount: 0, isAddView: false}
+
+  onAddButtonClicked = () => {
+    this.setState(prevState => ({
+      itemCount: prevState.itemCount + 1,
+      isAddView: !prevState.isAddView,
+    }))
+  }
+
+  onIncrement = () => {
+    this.setState(prevState => ({
+      itemCount: prevState.itemCount + 1,
+    }))
+  }
+
+  onDecrement = () => {
+    const {itemCount} = this.state
+    if (itemCount > 1) {
+      this.setState(prevState => ({
+        itemCount: prevState.itemCount - 1,
+      }))
+    } else {
+      this.setState(prevState => ({
+        isAddView: !prevState.isAddView,
+      }))
+    }
+  }
+
+  render() {
+    return (
+      <CartContext.Consumer>
+        {value => {
+          const {cartList, addCartItem} = value
+          console.log(cartList)
+          const {eachContent} = this.props
+          const {itemCount, isAddView} = this.state
+          return (
+            <li className="each-restaruant-food-list-item">
+              <img
+                src={eachContent.imageUrl}
+                alt={eachContent.name}
+                className="each-food-item-image"
+              />
+              <div className="item-details">
+                <h2 className="item-name">{eachContent.name}</h2>
+                <p className="item-cost">₹ {eachContent.cost}</p>
+                <span className="item-rating-container">
+                  <img
+                    src="https://res.cloudinary.com/dcqt2hg87/image/upload/v1703165215/star_rktiju.png"
+                    alt="rating"
+                  />
+                  <h2 className="item-rating">{eachContent.rating}</h2>
+                </span>
+                {isAddView ? (
+                  <div className="add-sub-button-container">
+                    <button
+                      type="button"
+                      onClick={this.onDecrement}
+                      className="alter-buttons"
+                    >
+                      -
+                    </button>
+                    <div className="item-count-text">{itemCount}</div>
+                    <button
+                      type="button"
+                      onClick={this.onIncrement}
+                      className="alter-buttons"
+                    >
+                      +
+                    </button>
+                  </div>
+                ) : (
+                  <button
+                    className="add-button"
+                    type="button"
+                    onClick={this.onAddButtonClicked}
+                  >
+                    ADD
+                  </button>
+                )}
+              </div>
+            </li>
+          )
+        }}
+      </CartContext.Consumer>
+    )
+  }
 }
 
 export default EachRestaurantFoodItem
