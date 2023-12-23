9acaa9a6f753049d8f0aed921fbd4976c5991dae
Ankireddy2811 <ankireddyputtam@gmail.com>
2023-12-23 10:42:27 +0000

    add styling for cartitem

--- src/components/CartListView/index.js
+++ src/components/CartListView/index.js
@@ -9,6 +9,20 @@ const CartListView = () => (
       const {cartList} = value
       return (
         <ul className="cart-list">
+          <li className="header-cart-item">
+            <div className="header-cart-item-details-container">
+              <div className="header-cart-product-title-brand-container">
+                <p className="header-cart-product-title">Item</p>
+              </div>
+              <div className="header-cart-quantity-container">
+                <p className="header-cart-quantity">Quantity</p>
+              </div>
+              <div className="header-total-price-container">
+                <p className="header-cart-total-price">Price</p>
+              </div>
+            </div>
+          </li>
+
           {cartList.map(eachCartItem => (
             <CartItem key={eachCartItem.id} cartItemDetails={eachCartItem} />
           ))}
--- src/components/EmptyCartView/index.js
+++ src/components/EmptyCartView/index.js
@@ -1,23 +1,33 @@
-import {Link} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 
 import './index.css'
 
-const EmptyCartView = () => (
-  <div className="cart-empty-view-container">
-    <img
-      src="https://res.cloudinary.com/dcqt2hg87/image/upload/v1703322378/cooking_1_jurppp.png"
-      className="cart-empty-image"
-      alt="cart empty"
-    />
-    <h1 className="cart-empty-heading">No Orders Yet!</h1>
-    <p className="cart-empty-para">
-      Your cart is empty. Add something from the menu.
-    </p>
+const EmptyCartView = props => {
+  const onOrderNowButton = () => {
+    const {history} = props
+    history.replace('/')
+  }
+  return (
+    <div className="cart-empty-view-container">
+      <img
+        src="https://res.cloudinary.com/dcqt2hg87/image/upload/v1703322378/cooking_1_jurppp.png"
+        className="cart-empty-image"
+        alt="cart empty"
+      />
+      <h1 className="cart-empty-heading">No Orders Yet!</h1>
+      <p className="cart-empty-para">
+        Your cart is empty. Add something from the menu.
+      </p>
 
-    <button type="button" className="order-now-btn">
-      Order Now
-    </button>
-  </div>
-)
+      <button
+        type="button"
+        className="order-now-btn"
+        onClick={onOrderNowButton}
+      >
+        Order Now
+      </button>
+    </div>
+  )
+}
 
-export default EmptyCartView
+export default withRouter(EmptyCartView)
